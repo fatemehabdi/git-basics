d5afbcfcc0e0e89f02e0e9c68f9917aa7a1e903a
fatemehabdi <fatemeh.abdi.elec@gmail.com>
2020-12-06 12:46:53 +0100

    update

---
+++ Simple ANC/FIR Filter/Readme.vhd
@@ -0,0 +1,20 @@
+---title new code 
+---- new project accelleration
+entity 
+port(a in )
+end; 
+
+Git Basics
+-------------
+-- new changes in code 
+ 
+if a> basics
+
+then 
+
+while 1 = abs
+ end 
+if  a> 1 then 
+else 
+end
+
