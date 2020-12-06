a1e950053fdc19c55c49278a768b2f3a33d5e8b9
fatemehabdi <fatemeh.abdi.elec@gmail.com>
2020-12-06 12:53:44 +0100

    updated

---
+++ Simple ANC/VSS LMS/Readme.vhd
@@ -0,0 +1,20 @@
+---title new code 
+---- new project accelleration
+entity 
+
+end; 
+
+Git Basics
+-------------
+
+ 
+if a> basics
+ c <= a+b ; 
+then 
+
+while 1 = abs
+ end 
+if  a> 1 then 
+else 
+end
+
